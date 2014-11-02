ec9bac9436676735014a886755756c2dcd697df9
Jhoterdan de Paula <jotaskt@gmail.com>
2014-11-02 17:10:32 -0200

    update ignore file

--- config/instagramConfig.js
+++ config/instagramConfig.js
@@ -1,7 +1,7 @@
 // insert data of your application (for instagram) 
 module.exports = {
-	'CLIENT_ID'			:   '',
-	'CLIENT_SECRET'		:	'',
-	'WEBSITE_URL'		:	'',
-	'REDIRECT_URI'		:	''
+	'CLIENT_ID'			:   'c074e949976b423d8ae7c52ea97e6617',
+	'CLIENT_SECRET'		:	'dfa58ceb26df40ff914c8fdfd357b6cd',
+	'WEBSITE_URL'		:	'https://github.com/jotadepaula/node-social-search',
+	'REDIRECT_URI'		:	'http://localhost:9000/instagram/search'
 }
