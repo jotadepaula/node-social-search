fbad0c768c78a0d1af11c3fb07e24826d70f780f
Jhoterdan de Paula <jotaskt@gmail.com>
2014-11-02 17:11:54 -0200

    remove configs

--- config/instagramConfig.js
+++
@@ -1,7 +0,0 @@
-// insert data of your application (for instagram) 
-module.exports = {
-	'CLIENT_ID'			:   'c074e949976b423d8ae7c52ea97e6617',
-	'CLIENT_SECRET'		:	'dfa58ceb26df40ff914c8fdfd357b6cd',
-	'WEBSITE_URL'		:	'https://github.com/jotadepaula/node-social-search',
-	'REDIRECT_URI'		:	'http://localhost:9000/instagram/search'
-}
