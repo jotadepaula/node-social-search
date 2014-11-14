959e2bea1fe731f500d136ebe076c0a92c81be93
Jhoterdan de Paula <jotaskt@gmail.com>
2014-11-14 20:16:44 -0200

    search google plus api OK. Created GoogleService lib

--- app/GoogleManager.js
+++ app/GoogleManager.js
@@ -1,14 +1,14 @@
-module.exports = function (app) {
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
-	var Client = require('node-rest-client').Client;
-	client = new Client();
+module.exports = function (app) {
+//google = require('GoogleService');
+
 
 	app.get('/google/search',function(req,res){
-		client.get('https://www.googleapis.com/plus/v1/activities?query=belfie&orderBy=best&key=AIzaSyCmfUNZwWzZTsL2UF8kUB7OaYlUGWevpVc',function(data,response){
-			if(data){
-				console.log('GET GOOGLE SEARCH');
-				res.send(data);
-			}
-		});
+		//https://www.googleapis.com/plus/v1/activities?query=belfie&orderBy=best&key=AIzaSyAN0xwSZ0T2_FYwblJVw7qNuM_ez4kmiXA
+        client.get("https://www.googleapis.com/plus/v1/activities?query=belfie&orderBy=best&key=AIzaSyAN0xwSZ0T2_FYwblJVw7qNuM_ez4kmiXA",function(data,response){
+            res.send(data);
+        });
 	});
-}
+};
